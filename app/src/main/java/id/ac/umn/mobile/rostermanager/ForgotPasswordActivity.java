ae8661e19d0561088316f3462852e61fe5779497
gnmarvin <nathaneilmarvin96@gmail.com>
2018-05-14 17:05:42 +0900

    Initial commit

---
+++ app/src/main/java/id/ac/umn/mobile/rostermanager/ForgotPasswordActivity.java
@@ -0,0 +1,45 @@
+package id.ac.umn.mobile.rostermanager;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+
+        final TextView emailText = (TextView) findViewById(R.id.email);
+        final Button sendRequestButton = (Button) findViewById(R.id.send_request);
+        sendRequestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DisplayToast(emailText.getText().toString());
+                if (emailText.getText().toString() == "") {
+                    DisplayToast("Please fill your email");
+                }
+                else if (emailText.getText().toString() == "admin@admin") {
+                    DisplayToast("Check your email to fix your account");
+                }
+                else {
+                    DisplayToast("Account not found");
+                }
+            }
+        });
+    }
+
+    private void DisplayToast(String pesan) {
+        Toast.makeText(getBaseContext(), pesan, Toast.LENGTH_SHORT).show();
+    }
+
+}
---
+++ app/src/main/java/id/ac/umn/mobile/rostermanager/LoginActivity.java
@@ -0,0 +1,35 @@
+package id.ac.umn.mobile.rostermanager;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        final Button loginButton = (Button) findViewById(R.id.login);
+        final TextView forgotText = (TextView) findViewById(R.id.forgot);
+
+        forgotText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
+                startActivity(i);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
