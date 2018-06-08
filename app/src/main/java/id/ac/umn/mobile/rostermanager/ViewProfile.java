1cc90d845bad5a611c5328b2368c018c4839df52
gnmarvin <nathaneilmarvin96@gmail.com>
2018-06-08 20:55:35 +0900

    qr code contact_id

--- app/src/main/java/id/ac/umn/mobile/rostermanager/ViewProfile.java
+++ app/src/main/java/id/ac/umn/mobile/rostermanager/ViewProfile.java
@@ -1,13 +1,22 @@
 package id.ac.umn.mobile.rostermanager;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.app.Dialog;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.encoder.ByteMatrix;
 
 public class ViewProfile extends AppCompatActivity {
     String full_name = "";               //variable buat nampung username inputan user
@@ -15,6 +24,7 @@ public class ViewProfile extends AppCompatActivity {
     String mobile_no = "";
     String mobile_whatsapp = "";
     String mobile_line = "";
+    String contact_id = "";
     TextView name_this, email_this, phone_this, line_this, whatsapp_this;
 
     @Override
@@ -22,6 +32,7 @@ public class ViewProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_profile);
 
+        //Get the data from main activity
         Intent i = getIntent();
         Bundle extras = i.getExtras();
         full_name = extras.getString("USERNAME");
@@ -29,6 +40,29 @@ public class ViewProfile extends AppCompatActivity {
         mobile_no = extras.getString("MOBILE_NO");
         mobile_whatsapp = extras.getString("MOBILE_WHATSAPP");
         mobile_line = extras.getString("MOBILE_LINE");
+        contact_id = extras.getString("CONTACT_ID");
+
+        //QR code generator
+        QRCodeWriter writer = new QRCodeWriter();
+        ImageView contactIdQr = (ImageView) findViewById(R.id.qr_code_image);
+        try {
+            BitMatrix bitMatrix = writer.encode(contact_id, BarcodeFormat.QR_CODE, 512, 512);
+                    int width = 512;
+                    int height = 512;
+                    Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+                    for (int x = 0; x < width; x++) {
+                        for (int y = 0; y < height; y++) {
+                            if (!bitMatrix.get(x, y)) {
+                                bmp.setPixel(x, y, Color.WHITE);
+                            } else {
+                                bmp.setPixel(x, y, Color.BLACK);
+                            }
+                        }
+                    }
+                    contactIdQr.setImageBitmap(bmp);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
 
         name_this = findViewById(R.id.name);
         name_this.setText(full_name);
