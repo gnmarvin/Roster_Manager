2f9f008b09b23d855d9e33d7ed7ca890e767c00a
gnmarvin <nathaneilmarvin96@gmail.com>
2018-06-08 18:13:05 +0900

    json event

--- app/src/main/java/id/ac/umn/mobile/rostermanager/PlanEventsFragment.java
+++ app/src/main/java/id/ac/umn/mobile/rostermanager/PlanEventsFragment.java
@@ -6,13 +6,20 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.telecom.Call;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class PlanEventsFragment extends Fragment {
     private RecyclerView mRecyclerView;
@@ -33,57 +40,94 @@ public class PlanEventsFragment extends Fragment {
         LinearLayoutManager llm = new LinearLayoutManager(getActivity());
         rv.setLayoutManager(llm);
 
-        planEventList = new ArrayList<>();
+        APIService webServiceAPI = APIClient.getApiClient().create(APIService.class);
+        retrofit2.Call<JsonElement> listEvent = webServiceAPI.Event();
 
-        //input data, gunakan loop untuk mengambil data dari database
-        planEventList.add(
-                new PlanEventsModel(
-                        "Sunday Service",
-                        "Sunday, 20 May 2018",
-                        "08.00-10.00",
-                        "Aldy Stevanus",
-                        "Team D",
-                        "3/10",
-                        "Team A",
-                        "8/10"));
-        planEventList.add(
-                new PlanEventsModel(
-                        "Sunday Service",
-                        "Sunday, 20 May 2018",
-                        "10.30-12.30",
-                        "Aldy Stevanus",
-                        "Team D",
-                        "5/10",
-                        "Team A",
-                        "8/10"));
-        planEventList.add(
-                new PlanEventsModel(
-                        "Sunday Service",
-                        "Sunday, 20 May 2018",
-                        "17.00-19.00",
-                        "Yosua Aryo Putra",
-                        "Team B",
-                        "2/10",
-                        "Team B",
-                        "8/10"));
-        planEventList.add(
-                new PlanEventsModel(
-                        "She",
-                        "Sunday, 2 May 2018",
-                        "17.00-19.00",
-                        "Yosua Aryo Putra",
-                        "Team B",
-                        "0/1",
-                        "Team B",
-                        "1/1"));
+        listEvent.enqueue(new Callback<JsonElement>() {
+            @Override
+            public void onResponse(retrofit2.Call<JsonElement> call, Response<JsonElement> response) {
+                JsonElement element = response.body();
+                JsonObject obj = element.getAsJsonObject();
+                String event_name  = obj.get("event_name").getAsString();
+                String event_start_date = obj.get("event_start_date").getAsString();
+                String event_end_date = obj.get("event_end_date").getAsString();
+                String event_type = obj.get("event_type").getAsString();
+
+                //INI CONTOH LOOP BUAT BACA JSON AMPE ABIS
+//                JsonElement element = response.body();
+//                JsonObject obj = element.getAsJsonObject();
+//                JsonArray data = obj.get("result").getAsJsonArray();
+//
+//                for(int i = 0 ; i < data.size() ; i++){
+//                    JsonObject singleData = data.get(i).getAsJsonObject();
+//                    String idArtwork = singleData.get("IDArtWork").getAsString();
+//                    String titleArtwork = singleData.get("Title").getAsString();
+//                    String fileArtwork = singleData.get("DirectoryData").getAsString();
+//                    String idArtist = singleData.get("IDArtist").getAsString();
+//                    String nameArtist = singleData.get("DisplayName").getAsString();
+//                    String emailArtist = singleData.get("EMail").getAsString();
+//
+//                    ModelArtworkInformation dataArtworkToList = new ModelArtworkInformation(idArtwork, titleArtwork, idArtist, nameArtist, emailArtist, fileArtwork);
+//                    artworkList.add(dataArtworkToList);
+//                }
+
+                planEventList = new ArrayList<>();
 
 
-        PlanEventsAdapter adapter = new PlanEventsAdapter(getContext(), planEventList);
 
-        rv.setAdapter(adapter);
+                //input data, gunakan loop untuk mengambil data dari database
+                planEventList.add(
+                        new PlanEventsModel(
+                                event_name,
+                                event_start_date,
+                                "08.00-10.00",
+                                "Aldy Stevanus",
+                                "Team D",
+                                "3/10",
+                                "Team A",
+                                "8/10"));
+                planEventList.add(
+                        new PlanEventsModel(
+                                "Sunday Service",
+                                "Sunday, 20 May 2018",
+                                "10.30-12.30",
+                                "Aldy Stevanus",
+                                "Team D",
+                                "5/10",
+                                "Team A",
+                                "8/10"));
+                planEventList.add(
+                        new PlanEventsModel(
+                                "Sunday Service",
+                                "Sunday, 20 May 2018",
+                                "17.00-19.00",
+                                "Yosua Aryo Putra",
+                                "Team B",
+                                "2/10",
+                                "Team B",
+                                "8/10"));
+                planEventList.add(
+                        new PlanEventsModel(
+                                "She",
+                                "Sunday, 2 May 2018",
+                                "17.00-19.00",
+                                "Yosua Aryo Putra",
+                                "Team B",
+                                "0/1",
+                                "Team B",
+                                "1/1"));
 
+                PlanEventsAdapter adapter = new PlanEventsAdapter(getContext(), planEventList);
+
+                rv.setAdapter(adapter);
+
+            }
+            @Override
+            public void onFailure(retrofit2.Call<JsonElement> call, Throwable t) {
+
+            }
+        });
         return rootView;
-
     }
 
 
